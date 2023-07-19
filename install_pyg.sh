11c2126e8d3c8bf196599591d2035ffe8f439a08
michellemli <limichelle1995@gmail.com>
2023-07-19 18:05:51 -0400

    Update env

---
+++ install_pyg.sh
@@ -0,0 +1,3 @@
+pip install torch-scatter==2.0.9 -f https://pytorch-geometric.com/whl/torch-1.12.1+cu102.html
+pip install torch-sparse==0.6.15 -f https://pytorch-geometric.com/whl/torch-1.12.1+cu102.html
+pip install torch-geometric==2.1.0
