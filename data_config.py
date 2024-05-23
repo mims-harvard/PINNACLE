e92c05cc0d3b2bb9c4d72d6d24e1e044925d48ad
michellemli <limichelle1995@gmail.com>
2024-05-23 10:41:45 -0400

    Fix paths

--- data_config.py
+++ data_config.py
@@ -1,24 +1,24 @@
 ############################################################
 # DATA DIRECTORY
 
-DATA_DIR = "../data/"
-RAW_DATA_DIR = DATA_DIR + "raw/"
-OUTPUT_DIR = DATA_DIR + "networks/"
+DATA_DIR = "../data/"                                       # Data directory
+RAW_DATA_DIR = DATA_DIR + "raw/"                            # Directory with all draw data
+OUTPUT_DIR = DATA_DIR + "networks/"                         # Directory with all networks
 
 
 ############################################################
 # DATA FILES
 
 # Single cell transcriptomic atlas
-TABULA_SAPIENS_DIR = RAW_DATA_DIR + "TabulaSapiens.h5ad"
-TS_TISSUE_DATA_DIR = RAW_DATA_DIR + "ts_data_tissue.csv"
+TABULA_SAPIENS_DIR = RAW_DATA_DIR + "TabulaSapiens.h5ad"    # Single-cell atlas
+TS_TISSUE_DATA_DIR = OUTPUT_DIR + "ts_data_tissue.csv"      # Generated data_prep/presave_compartments.py
 
 # Global PPI network
-PPI_DIR = OUTPUT_DIR + "global_ppi_edgelist.txt"
-METAGRAPH_DIR = OUTPUT_DIR + "mg_edgelist.txt"
+PPI_DIR = OUTPUT_DIR + "global_ppi_edgelist.txt"            # Global PPI network
+METAGRAPH_DIR = OUTPUT_DIR + "mg_edgelist.txt"              # Metagraph
 
 ############################################################
 # AUXILIARY DATA FILES
 
-CELL_ONTOLOGY_DIR = DATA_DIR + "cell-ontology/cl-full.obo"
-BTO_DIR = RAW_DATA_DIR + "BTO.obo"
+CELL_ONTOLOGY_DIR = DATA_DIR + "cell-ontology/cl-full.obo"  # Cell ontology
+BTO_DIR = RAW_DATA_DIR + "BTO.obo"                          # Tissue ontology
--- data_prep/presave_compartments.py
+++ data_prep/presave_compartments.py
@@ -2,10 +2,10 @@ from utils import read_ts_data
 
 import sys
 sys.path.insert(0, '..') # add data_config to path
-from data_config import TABULA_SAPIENS_DIR, OUTPUT_DIR
+from data_config import TABULA_SAPIENS_DIR, TS_TISSUE_DATA_DIR
 
 
 ts_data = read_ts_data(TABULA_SAPIENS_DIR)
 print(ts_data)
 print(ts_data.obs[["anatomical_information", "organ_tissue", "compartment", "cell_ontology_class"]])
-ts_data.obs.to_csv(OUTPUT_DIR + "ts_data_tissue.csv", sep = "\t")
+ts_data.obs.to_csv(TS_TISSUE_DATA_DIR, sep = "\t")
